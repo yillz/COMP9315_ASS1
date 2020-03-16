f35e4f8eba6cbc163d45f0003d14c1c24a29cbe8
Mark_Ma <50102783+MarkMaMsw@users.noreply.github.com>
2020-03-16 19:48:35 +1100

    modify pname_in and struct name
    modify pname_in function
    rename the struct pName->PersonName

--- pname.c
+++ pname.c
@@ -19,18 +19,17 @@
 
 PG_MODULE_MAGIC;
 
-typedef struct pName
+typedef struct PersonName
 {
     int length;
+	// char familyName[FLEXIBLE_ARRAY_MEMBER];
+	// char givenName[FLEXIBLE_ARRAY_MEMBER];
+	char *familyName;
+	char *givenName;
+} PersonName;
 
-	char familyName[FLEXIBLE_ARRAY_MEMBER];
-	char givenName[FLEXIBLE_ARRAY_MEMBER];
-} pName;
 
-// static int checkName(char *name);
-
-// static int compareNames(pName *a, pName *b);
-
+// define a function to check if name is valid
 static bool checkName(char *name){
 	int cflags = REG_EXTENDED;
 	regex_t reg;
@@ -69,7 +68,7 @@ pname_in(PG_FUNCTION_ARGS)
 {
 	char *NameIn = PG_GETARG_CSTRING(0);
 	char familyName[100], 
-		 givenName[100];
+		 givenName[100],
 		 punct[10];
 
 	if (checkName(NameIn) == false){
@@ -88,22 +87,22 @@ pname_in(PG_FUNCTION_ARGS)
 	int32 new_pname_size = strlen(familyName) + strlen(givenName) + 2;
 
 	// assign memory to result
-	pName *result = (pName *)palloc(VARHDRSZ + new_pname_size);
+	PersonName *result = (PersonName *)palloc(VARHDRSZ + new_pname_size);
 
 	// set VARSIZE
 	SET_VARSIZE(result, VARHDRSZ + new_pname_size);
 
 	// memcopy
-	memcpy(VARDATA(result), VARDATA_ANY(familyName), (strlen(familyName) + 1));
-	memcpy(VARDATA(result) + strlen(familyName) + 1, VARDATA_ANY(givenName), (strlen(givenName) + 1));
+	// memcpy(VARDATA(result), VARDATA_ANY(familyName), (strlen(familyName) + 1));
+	// memcpy(VARDATA(result) + strlen(familyName) + 1, VARDATA_ANY(givenName), (strlen(givenName) + 1));
 
 	// locate pointer
 	result->familyName = result + VARHDRSZ；
 	result->givenName = result->familyName + strlen(familyName) + 1；
 	
 	// copy familyName and givenName to result
-	//strcpy(result->familyName, familyName);
-	//strcpy(result->givenName, givenName);
+	strcpy(result->familyName, familyName);
+	strcpy(result->givenName, givenName);
 	PG_RETURN_POINTER(result);
 }
 
@@ -114,7 +113,7 @@ PG_FUNCTION_INFO_V1(pname_out);
 Datum
 pname_out(PG_FUNCTION_ARGS)
 {
-	pName *fullname = (pName *) PG_GETARG_POINTER(0);
+	PersonName *fullname = (PersonName *) PG_GETARG_POINTER(0);
 	char  *result;
 
 	result = psprintf("%s,%s", fullname->familyName, fullname->givenName);
@@ -135,7 +134,7 @@ pname_out(PG_FUNCTION_ARGS)
  *****************************************************************************/
 
 // function to compare strings
-static int compareNames(pName *a, pName *b){
+static int compareNames(PersonName *a, PersonName *b){
 	//waiting for Mark 
 	int result = strcmp(a->familyName, b->familyName);
 
@@ -152,8 +151,8 @@ PG_FUNCTION_INFO_V1(pname_equal);
 Datum
 pname_equal(PG_FUNCTION_ARGS)
 {
-	pName    *a = (pName *) PG_GETARG_POINTER(0);
-	pName    *b = (pName *) PG_GETARG_POINTER(1);
+	PersonName    *a = (PersonName *) PG_GETARG_POINTER(0);
+	PersonName    *b = (PersonName *) PG_GETARG_POINTER(1);
 
 	PG_RETURN_BOOL(compareNames(a, b) == 0);
 }
@@ -164,8 +163,8 @@ PG_FUNCTION_INFO_V1(pname_greater);
 Datum
 pname_greater(PG_FUNCTION_ARGS)
 {
-	pName    *a = (pName *) PG_GETARG_POINTER(0);
-	pName    *b = (pName *) PG_GETARG_POINTER(1);
+	PersonName    *a = (PersonName *) PG_GETARG_POINTER(0);
+	PersonName    *b = (PersonName *) PG_GETARG_POINTER(1);
 
 	PG_RETURN_BOOL(compareNames(a, b) > 0);
 }
@@ -176,8 +175,8 @@ PG_FUNCTION_INFO_V1(pname_not_equal);
 Datum
 pname_not_equal(PG_FUNCTION_ARGS)
 {
-	pName    *a = (pName *) PG_GETARG_POINTER(0);
-	pName    *b = (pName *) PG_GETARG_POINTER(1);
+	PersonName    *a = (PersonName *) PG_GETARG_POINTER(0);
+	PersonName    *b = (PersonName *) PG_GETARG_POINTER(1);
 
 	PG_RETURN_BOOL(compareNames(a, b) != 0);
 }
@@ -188,8 +187,8 @@ PG_FUNCTION_INFO_V1(pname_greater_equal);
 Datum
 pname_greater_equal(PG_FUNCTION_ARGS)
 {
-	pName    *a = (pName *) PG_GETARG_POINTER(0);
-	pName    *b = (pName *) PG_GETARG_POINTER(1);
+	PersonName    *a = (PersonName *) PG_GETARG_POINTER(0);
+	PersonName    *b = (PersonName *) PG_GETARG_POINTER(1);
 
 	PG_RETURN_BOOL(compareNames(a, b) >= 0);
 }
@@ -200,8 +199,8 @@ PG_FUNCTION_INFO_V1(pname_less);
 Datum
 pname_less(PG_FUNCTION_ARGS)
 {
-	pName    *a = (pName *) PG_GETARG_POINTER(0);
-	pName    *b = (pName *) PG_GETARG_POINTER(1);
+	PersonName    *a = (PersonName *) PG_GETARG_POINTER(0);
+	PersonName    *b = (PersonName *) PG_GETARG_POINTER(1);
 
 	PG_RETURN_BOOL(compareNames(a, b) < 0);
 }
@@ -212,8 +211,8 @@ PG_FUNCTION_INFO_V1(pname_less_equal);
 Datum
 pname_less_equal(PG_FUNCTION_ARGS)
 {
-	pName    *a = (pName *) PG_GETARG_POINTER(0);
-	pName    *b = (pName *) PG_GETARG_POINTER(1);
+	PersonName    *a = (PersonName *) PG_GETARG_POINTER(0);
+	PersonName    *b = (PersonName *) PG_GETARG_POINTER(1);
 
 	PG_RETURN_BOOL(compareNames(a, b) <= 0);
 }
